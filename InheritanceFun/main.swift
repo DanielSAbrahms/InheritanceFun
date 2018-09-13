53adb62bc6205f8678c3b78d1b91711cb4ebda40
Abrahms, Daniel Shane <dabrahms@hk223-27704.local>
2018-09-13 10:12:09 -0700

    Initial Commit

---
+++ InheritanceFun/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  InheritanceFun
+//
+//  Created by Abrahms, Daniel Shane on 9/13/18.
+//  Copyright © 2018 Abrahms, Daniel Shane. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
